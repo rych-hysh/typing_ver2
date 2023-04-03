2977812a2e1725ea428c9efc40728807b8f475a0
rhayashi <ryo.robolabo@gmail.com>
2023-04-03 20:58:50 +0900

    commit able to multi consonant

--- src/util/util.js
+++ src/util/util.js
@@ -6,64 +6,64 @@ function getConsonant(kana) {
 		case "く":
 		case "け":
 		case "こ":
-			return "k"
+			return ["k"]
 		case "さ":
 		case "し":
 		case "す":
 		case "せ":
 		case "そ":
-			return "s"
+			return ["s"]
 		case "た":
 		case "ち":
 		case "つ":
 		case "て":
 		case "と":
-			return "t"
+			return ["t"]
 		case "な":
 		case "に":
 		case "ぬ":
 		case "ね":
 		case "の":
-			return "n"
+			return ["n"]
 
 		case "は":
 		case "ひ":
 		case "ふ":
 		case "へ":
 		case "ほ":
-			return "h"
+			return ["h"]
 		case "ま":
 		case "み":
 		case "む":
 		case "め":
 		case "も":
-			return "m"
+			return ["m"]
 
 		case "や":
 		case "ゆ":
 		case "よ":
-			return "y"
+			return ["y"]
 
 		case "ら":
 		case "り":
 		case "る":
 		case "れ":
 		case "ろ":
-			return "r"
+			return ["r"]
 		case "わ":
 		case "を":
-			return "w"
+			return ["w"]
 		case "が":
 		case "ぎ":
 		case "ぐ":
 		case "げ":
 		case "ご":
-			return "g"
+			return ["g"]
 		case "ざ":
 		case "ず":
 		case "ぜ":
 		case "ぞ":
-			return "z"
+			return ["z"]
 		case "じ":
 			return ["j", "z"]
 		case "だ":
@@ -71,7 +71,13 @@ function getConsonant(kana) {
 		case "づ":
 		case "で":
 		case "ど":
-			return "d"
+			return ["d"]
+		case "ば":
+		case "び":
+		case "ぶ":
+		case "べ":
+		case "ぼ":
+			return ["b"]
 		default:
 			break;
 	}
