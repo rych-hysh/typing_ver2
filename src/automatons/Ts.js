326c07c51781c37c302e316c24c4a43f79f3d2ca
rhayashi <ryo.robolabo@gmail.com>
2023-04-03 17:29:32 +0900

    implements automaton da - do

--- src/automatons/Ts.js
+++ src/automatons/Ts.js
@@ -108,6 +108,79 @@ function automaton_DA(input){
 					return ["miss", 0];
 	}
 }
+function automaton_DI(input) {
+	switch (state) {
+			case "q_init":
+					if (input == "d") {
+							state = "q_1"
+							return ["hit", 0];
+					}
+					return ["miss", 0];
+			case "q_1":
+					if (input == "i") {
+							state = "q_exit";
+							return ["hit", 1];
+					}
+					return ["miss", 0]
+			default:
+					return ["miss", 0];
+	}
+}
+function automaton_DU(input) {
+	switch (state) {
+			case "q_init":
+					if (input == "d") {
+							state = "q_1"
+							return ["hit", 0];
+					}
+					return ["miss", 0];
+			case "q_1":
+					if (input == "u") {
+							state = "q_exit";
+							return ["hit", 1];
+					}
+					return ["miss", 0]
+			default:
+					return ["miss", 0];
+	}
+}
+function automaton_DE(input) {
+	switch (state) {
+			case "q_init":
+					if (input == "d") {
+							state = "q_1"
+							return ["hit", 0];
+					}
+					return ["miss", 0];
+			case "q_1":
+					if (input == "e") {
+							state = "q_exit";
+							return ["hit", 1];
+					}
+					return ["miss", 0]
+			default:
+					return ["miss", 0];
+	}
+}
+
+function automaton_DO(input) {
+	switch (state) {
+			case "q_init":
+					if (input == "d") {
+							state = "q_1"
+							return ["hit", 0];
+					}
+					return ["miss", 0];
+			case "q_1":
+					if (input == "o") {
+							state = "q_exit";
+							return ["hit", 1];
+					}
+					return ["miss", 0]
+			default:
+					return ["miss", 0];
+	}
+}
 
 function automaton_LTU(input){
 	switch (state) {
