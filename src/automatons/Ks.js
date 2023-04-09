32f9043df8b12105313a17d8800876167ea07a48
hayar <ryo.robolabo@gmail.com>
2023-04-10 01:46:54 +0900

    implement gya,gyu,gyo

--- src/automatons/Ks.js
+++ src/automatons/Ks.js
@@ -280,3 +280,92 @@ function automaton_KYO(input) {
 			return ["miss", 0];
 	}
 }
+
+function automaton_GYA(input) {
+	switch (state) {
+		case "q_init":
+			if (input == "g") {
+				state = "q_1"
+				return ["hit", 0]
+			}
+			return ["miss", 0]
+		case "q_1":
+			if (input == "y") {
+				state = "q_2"
+				return ["hit", 0]
+			}
+			if (input == "i") {
+				state = "q_exit"
+				return ["hit", 1]
+			}
+			return ["miss", 0]
+		case "q_2":
+			if (input == "a") {
+				state = "q_exit"
+				return ["hit", 2]
+			}
+			return ["miss", 0]
+		default:
+			console.log("unexpected error");
+			return ["miss", 0];
+	}
+}
+function automaton_GYU(input) {
+	switch (state) {
+		case "q_init":
+			if (input == "g") {
+				state = "q_1"
+				return ["hit", 0]
+			}
+			return ["miss", 0]
+		case "q_1":
+			if (input == "y") {
+				state = "q_2"
+				return ["hit", 0]
+			}
+			if (input == "i") {
+				state = "q_exit"
+				return ["hit", 1]
+			}
+
+			return ["miss", 0]
+		case "q_2":
+			if (input == "u") {
+				state = "q_exit"
+				return ["hit", 2]
+			}
+			return ["miss", 0]
+		default:
+			console.log("unexpected error");
+			return ["miss", 0];
+	}
+}
+function automaton_GYO(input) {
+	switch (state) {
+		case "q_init":
+			if (input == "g") {
+				state = "q_1"
+				return ["hit", 0]
+			}
+			return ["miss", 0]
+		case "q_1":
+			if (input == "y") {
+				state = "q_2"
+				return ["hit", 0]
+			}
+			if (input == "i") {
+				state = "q_exit"
+				return ["hit", 1]
+			}
+
+			return ["miss", 0]
+		case "q_2":
+			if (input == "o") {
+				state = "q_exit"
+				return ["hit", 2]
+			}
+		default:
+			console.log("unexpected error");
+			return ["miss", 0];
+	}
+}
